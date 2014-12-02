23e87d713be838dd6d9bc78fb5ddeea7955ce6d3
saragiberson <saragiber@gmail.com>
2014-12-02 14:18:54 -0500

    authenticate with twitter

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,15 @@
+require 'pry'
 class User < ActiveRecord::Base
   has_many :user_events
   has_many :events, through: :user_events
 
   def self.create_with_omniauth(auth)
     create! do |user|
-      user.provider = auth["provider"]
-      user.uid = auth["uid"]
-      user.name = auth["user_info"]["name"]
+      user.provider          = auth["provider"]
+      user.uid               = auth["uid"]
+      user.name              = auth["info"]["name"]
+      user.twitter_handle    = auth["info"]["nickname"]
+      user.twitter_image_url = auth["info"]["image"]
     end
   end
 end
