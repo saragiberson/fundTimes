43a394d16896d1d7be301a9aa086d27a490e4b4b
saragiberson <saragiber@gmail.com>
2014-12-04 09:26:46 -0500

    created payment model

---
+++ app/models/payment.rb
@@ -0,0 +1,8 @@
+class Payment < ActiveRecord::Base
+
+  conn = Faraday.new(:url => 'https://api.venmo.com') do |faraday|
+    faraday.request  :url_encoded             # form-encode POST params
+    faraday.response :logger                  # log requests to STDOUT
+    faraday.adapter  Faraday.default_adapter  # make requests with Net::HTTP
+  end
+end
