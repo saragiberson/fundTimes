45b64a4a63273fb891ee491d95317ead19f600be
seemajune <seemashariat@gmail.com>
2014-12-04 22:46:18 -0500

    added logic to event show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,6 @@
 class UsersController < ApplicationController
-<<<<<<< HEAD
-  skip_before_action :venmo?, only: [:show, :venmo]
-=======
   skip_before_action :venmo?, only: [:venmo, :show]
->>>>>>> 635863d65e02b641725f93bb8c54d0702a753890
+
   def venmo
   end
 
